6c33f204e5df58d32e0d7fcc9fd40e63078ed5f4
Yiding Weng <wengx034@umn.edu.com>
2017-04-17 17:26:58 -0500

    audition

---
+++ nextEvens.js
@@ -0,0 +1,9 @@
+function nextEvens(startingNumber, numberOfEvens){
+	if (secondNumber >= firstNumber) || (step <= 0) {
+		document.write("Error")
+	}
+	else{
+		document.write("Good")
+		var numberResult = 100; 
+	}
+}
---
+++ samExercise.js
@@ -0,0 +1,9 @@
+function findEvens(firstNumber, secondNumber, step){
+	if (secondNumber >= firstNumber) || (step <= 0) {
+		document.write("Error")
+	}
+	else{
+		document.write("Good")
+		var numberResult = 100; 
+	}
+}
